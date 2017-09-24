e534b195ae500a99d54f38472ecbba632c62be41
Luis Felipe Hussin Bento <hussin@gmail.com>
2017-09-24 15:15:04 -0700

    new deploy

--- server.js
+++ server.js
@@ -1,50 +1,49 @@
 //  OpenShift sample Node application
-var express = require('express');
-var path = require('path');
-var app     = express();
-var morgan  = require('morgan');
+var express = require("express");
+var path = require("path");
+var app = express();
+var morgan = require("morgan");
 
-Object.assign = require('object-assign');
+Object.assign = require("object-assign");
 
-var routes = require('./routes/index');
+var routes = require("./routes/index");
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(morgan('combined'))
-app.use(express.static('dist'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+app.use(morgan("combined"));
+app.use(express.static("dist"));
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
+  mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+  mongoURLLabel = "";
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
+    mongoHost = process.env[mongoServiceName + "_SERVICE_HOST"],
+    mongoPort = process.env[mongoServiceName + "_SERVICE_PORT"],
+    mongoDatabase = process.env[mongoServiceName + "_DATABASE"],
+    mongoPassword = process.env[mongoServiceName + "_PASSWORD"];
+  mongoUser = process.env[mongoServiceName + "_USER"];
 
   if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
+    mongoURLLabel = mongoURL = "mongodb://";
     if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
+      mongoURL += mongoUser + ":" + mongoPassword + "@";
     }
     // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
+    mongoURLLabel += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
+    mongoURL += mongoHost + ":" + mongoPort + "/" + mongoDatabase;
   }
 }
 var db = null,
-    dbDetails = new Object();
+  dbDetails = {};
 
 var initDb = function(callback) {
   if (mongoURL == null) return;
 
-  var mongodb = require('mongodb');
+  var mongodb = require("mongodb");
   if (mongodb == null) return;
 
   mongodb.connect(mongoURL, function(err, conn) {
@@ -56,26 +55,26 @@ var initDb = function(callback) {
     db = conn;
     dbDetails.databaseName = db.databaseName;
     dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
+    dbDetails.type = "MongoDB";
 
-    console.log('Connected to MongoDB at: %s', mongoURL);
+    console.log("Connected to MongoDB at: %s", mongoURL);
   });
 };
 
-app.use('/', routes);
-app.use('/pebble-apps/timeboxed/config/', routes);
+app.use("/", routes);
+app.use("/pebble-apps/timeboxed/config/", routes);
 
 // error handling
-app.use(function(err, req, res, next){
+app.use(function(err, req, res, next) {
   console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+  res.status(500).send("Something bad happened!");
 });
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
+initDb(function(err) {
+  console.log("Error connecting to Mongo. Message:\n" + err);
 });
 
 app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+console.log("Server running on http://%s:%s", ip, port);
 
-module.exports = app ;
+module.exports = app;
